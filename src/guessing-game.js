a95223108cd8d09e5003952fe8f76cdfba155b3d
Pavel Nishcharetau <alzarlion@gmail.com>
2017-02-03 07:42:37 +0300

    Commit

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.solution = 0;
+        this.tmp = 0;
+    }
 
     setRange(min, max) {
-
+        this.solution = Math.round((min + max) / 2);
+        this.tmp = this.solution;
     }
 
     guess() {
-
+        return this.solution;
     }
 
     lower() {
-
+        this.tmp = Math.round(this.tmp) / 2;
+        this.solution -= Math.floor(this.tmp);
+        return this.solution;
     }
 
     greater() {
-
+        if(this.solution > this.tmp) {
+            this.tmp /= 2;
+        } else {
+            this.tmp = this.solution / 2;
+        }
+        this.solution += Math.round(this.tmp);
+        return this.solution;
     }
 }
 
